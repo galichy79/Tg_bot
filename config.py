87a54b89bf64e0b8cbd9c63ed2400c32d9b23e31
Yuriy Galych <galichy79@gmail.com>
2022-07-07 15:29:51 +0300

    Vivoditsya klaviatura

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = '5416757526:AAGehlX94QwdKGWyP359kCMyhqRDnJK9A2A'
+chat_id = 474041393
+URL_IPHONE13 = 'https://rozetka.com.ua/apple-iphone-13-mini-256gb-product-red/p318463306/'
+URL_MACBOOK = 'https://rozetka.com.ua/apple_macbook_pro_16_m1_pro_512gb_2021_space_gray/p322710583/'
---
+++ keyboards.py
@@ -0,0 +1,45 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.callback_data import CallbackData
+
+from config import URL_IPHONE13, URL_MACBOOK
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='btn1'),
+            KeyboardButton(text='btn2'),
+        ],
+        [
+            KeyboardButton(text='btn3'),
+        ]
+    ],
+    resize_keyboard=True
+)
+
+cb = CallbackData('buy', 'id', 'name', 'price')
+
+keyboard1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Iphone 13', callback_data='buy:0:phone:1000'),
+            InlineKeyboardButton(text='MacBook', callback_data='buy:1:mac:999999')
+        ],
+        [
+            InlineKeyboardButton(text='Cancel', callback_data='cancel')
+        ]
+    ]
+)
+phone_key = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton('Купить', url=URL_IPHONE13)
+        ]
+    ]
+)
+mak_key = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton('Купить', url=URL_MACBOOK)
+        ]
+    ]
+)
